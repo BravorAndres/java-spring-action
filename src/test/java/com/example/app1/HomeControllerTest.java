54ff009ef6c50828c3eae9c061d3420e5bb88400
Andres <bravor.andres24@gmail.com>
2022-06-08 18:23:30 -0500

    Problema de dependencias solucionado

--- src/test/java/com/example/app1/HomeControllerTest.java
+++ src/test/java/com/example/app1/HomeControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
     @Test
